7517a0bdbfa63bdf5a9eddc2d4cdad66318ff838
Stepan Chekan <darilldev@gmail.com>
2026-05-31 14:33:53 +0800

    feat(ui-kit/form-field): export form-field, slot directives and UiKitInputDirective from forms index

--- src/app/shared/ui-kit/forms/index.ts
+++ src/app/shared/ui-kit/forms/index.ts
@@ -1,3 +1,15 @@
 export { InputComponent } from './input/input.component';
 export { SelectComponent } from './select/select.component';
 export type { SelectOption } from './select/select.component';
+
+export { FormFieldComponent } from './form-field/form-field.component';
+export { FORM_FIELD_TOKEN } from './form-field/form-field.token';
+export type { IFormField, FormFieldApi } from './form-field/form-field.token';
+
+export { UiKitInputDirective } from './inputs/directives/ui-kit-input.directive';
+
+export { UiKitLabelDirective }  from './form-field/directives/ui-kit-label.directive';
+export { UiKitPrefixDirective } from './form-field/directives/ui-kit-prefix.directive';
+export { UiKitSuffixDirective } from './form-field/directives/ui-kit-suffix.directive';
+export { UiKitHintDirective }   from './form-field/directives/ui-kit-hint.directive';
+export { UiKitErrorDirective }  from './form-field/directives/ui-kit-error.directive';
